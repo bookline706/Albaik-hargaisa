b7fa208c06b0d9039fd3c6c77f74ccb7ddaa4c64
bookline706 <96940760+bookline706@users.noreply.github.com>
2022-02-07 14:27:08 -0330

    readme updated

---
+++ Albik.js
@@ -0,0 +1,36 @@
+
+let menu =document.querySelector('#menu-bars');
+let navbar =document.querySelector('.navbar');
+
+menu.onclick = () =>
+{
+    menu.classList.toggle('fa-times');
+    
+    navbar.classList.toggle('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+   spaceBetween:30,
+ centeredslides:true,
+autoplay:{
+     delay:1000,
+     desableOnInteration:false,
+   },
+pagination:
+  {
+     el: ".swiper-pagination",
+     clickable: true,
+    },
+    loop:true
+  });
+
+  function loader(){
+    document.querySelector('.lood').classList.add('fade-out');
+  
+  }
+  function fadeOut (){
+    setInterval(loader, 3000);
+  }
+  
+  window.onload = fadeOut;
+
